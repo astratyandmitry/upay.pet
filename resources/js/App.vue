ea8d03783e917a350dcb82f62f1bc242aec60ef3
astratyandmitry <astratyandmitry@gmail.com>
2023-02-02 17:10:59 +0600

    Frontend Init

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from "vue";
+
+const message = ref('Hello World!')
+</script>
+
+<template>
+    <h1 class="title">
+        {{ message }}
+    </h1>
+</template>
+
+<style lang="postcss" scoped>
+.title {
+    @apply text-center text-4xl text-gray-700;
+}
+</style>
